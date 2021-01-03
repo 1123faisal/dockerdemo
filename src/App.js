00bb29b8ab8c0fd7c8ad140c14773a75508b692a
1123faisal <faisalsistec@gmail.com>
2021-01-03 12:52:15 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>hello world</h1>
+        <h2>sdasasdasd</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
